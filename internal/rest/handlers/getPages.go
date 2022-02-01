33a650558b0b669c14c80680b67db12d7b175e86
Sergey <xxsmotur@gmail.com>
2022-02-01 22:31:09 +0500

    Add puppet for get handler

---
+++ internal/rest/handlers/getPages.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/saromanov/knowledge/internal/storage"
+)
+
+type GetPageHandler struct {
+	store storage.Storage
+}
+
+func NewGetArticleHandler(st storage.Storage) *CreatePageHandler {
+	return &GetPageHandler{
+		store: st,
+	}
+}
+func (h GetPageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
