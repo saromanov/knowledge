f88614ee3d52995aec8e62216a7729a94ec82265
Sergey <xxsmotur@gmail.com>
2022-02-12 10:15:17 +0500

    Adding logrus

--- cmd/knowledge/main.go
+++ cmd/knowledge/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/oklog/run"
+	"github.com/sirupsen/logrus"
 	"github.com/joeshaw/envdecode"
 	"github.com/saromanov/knowledge/internal/storage/postgres"
 	"github.com/saromanov/knowledge/internal/rest"
@@ -14,22 +15,28 @@ type config struct {
 	Postgres postgres.Config
 }
 func main(){
-	var cfg config
-	if err := envdecode.StrictDecode(&cfg); err != nil {
-		return
-	}
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	log := logrus.New().WithContext(ctx)
+	var cfg config
+	if err := envdecode.StrictDecode(&cfg); err != nil {
+		log.WithError(err).Fatal("unable to decode env variables")
+		return
+	}
 	g := &run.Group{}
 	pg := postgres.New(cfg.Postgres)
+	log.Info("Trying to initialize postgres")
 	if err := pg.Init(ctx); err != nil {
-		panic(err)
+		log.WithError(err).Fatal("unable to init postgres")
 	}
+	log.Info("Postgres initialized")
 	r := rest.New(rest.Config{
 		Address: "localhost:8044",
 	}, pg)
 
 	if err := service.StartService(ctx, r, g); err != nil {
-		panic(err)
+		log.WithError(err).Fatal("unable to start service")
 	}
+	log.Info("Finishing of the working")
 }
